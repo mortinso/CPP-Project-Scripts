fa926a4d9e8a20ff931cba4cd001f34d1985ccb7
mortinso <89762267+mortinso@users.noreply.github.com>
2024-06-05 13:13:40 +0100

    Added colors to header

--- newClass.sh
+++ newClass.sh
@@ -18,6 +18,15 @@ cat << EOF > $PWD/inc/$1.hpp
 
 # include <iostream>
 
+# define RED "\e[91m"
+# define GREEN "\e[92m"
+# define YELLOW "\e[93m"
+# define BLUE "\e[94m"
+# define PURPLE "\e[95m"
+# define CYAN "\e[96m"
+# define INVERT "\e[90m\e[107m"
+# define RESET "\e[0m"
+
 class $1 {
 	private:
 
