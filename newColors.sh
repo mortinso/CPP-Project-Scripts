e26a1a5cc569d0dbb8eb3b56f3b7d42f58b849b7
mortinso <89762267+mortinso@users.noreply.github.com>
2024-11-20 16:19:36 +0000

    Added a new header for colors

---
+++ newColors.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+# This script was created with the aim of creating a C++ header with colors to use in my project outputs.
+
+
+[ ! -d "$PWD/inc" ] && mkdir inc
+cat << EOF > $PWD/inc/colors.hpp
+#ifndef COLORS_HPP
+# define COLORS_HPP
+
+// Styles
+# define RESET			"\e[0m"
+# define BOLD			"\e[1m"
+# define ITALIC			"\e[3m"
+# define UNDERLINE		"\e[4m"
+# define STRIKETHROUGH	"\e[9m"
+
+// Regular Colors
+# define BLACK	"\e[30m"
+# define RED	"\e[31m"
+# define GREEN	"\e[32m"
+# define YELLOW	"\e[33m"
+# define BLUE	"\e[34m"
+# define PURPLE	"\e[35m
+# define CYAN	"\e[36m"
+# define WHITE	"\e[37m"
+
+// High Intensity Colors
+# define BLACK_H	"\e[90m"
+# define RED_H		"\e[91m"
+# define GREEN_H	"\e[92m"
+# define YELLOW_H	"\e[93m"
+# define BLUE_H		"\e[94m"
+# define PURPLE_H	"\e[95m"
+# define CYAN_H		"\e[96m"
+# define WHITE_H	"\e[97m"
+
+// Background
+# define BG_BLACK	"\e[40m"
+# define BG_RED		"\e[41m"
+# define BG_GREEN	"\e[42m"
+# define BG_YELLOW	"\e[43m"
+# define BG_BLUE	"\e[44m"
+# define BG_PURPLE	"\e[45m
+# define BG_CYAN	"\e[46m"
+# define BG_WHITE	"\e[47m"
+
+// High Intensity Background
+# define BG_BLACK_H		"\e[100m"
+# define BG_RED_H		"\e[101m"
+# define BG_GREEN_H		"\e[102m"
+# define BG_YELLOW_H	"\e[103m"
+# define BG_BLUE_H		"\e[104m"
+# define BG_PURPLE_H	"\e[105m
+# define BG_CYAN_H		"\e[106m"
+# define BG_WHITE_H		"\e[107m"
+
+#endif
+EOF
